2eb3fbe24c39e77d5bc6f762a16c45d545644ae4
Maurice Claessen <mc1984@gmail.com>
2025-08-09 11:38:28 +0200

    Add functionality and styling

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const PORT = 3000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-   res.render("index.ejs");
-})
+const API_URL = "https://api.chucknorris.io/jokes/random";
+
+app.use(express.static('public'));
+
+app.get("/", async (req, res) => {
+   try {
+      const response = await axios.get(API_URL);
+      res.render("index.ejs", { content: response.data });
+   }
+   catch (error) {
+      res.render("index.ejs", { error: JSON.stringify(error.response.data) });
+   }
+});
 
 app.listen(PORT, () => {
    console.log(`Server listening on port ${PORT}`);
